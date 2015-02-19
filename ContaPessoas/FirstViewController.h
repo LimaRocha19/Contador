7682b5f36bd49a6b5bb98b12cdb3d14eaf99c7ab
Isaías <isaiahlima18@gmail.com>
2015-02-19 08:53:22 -0200

    Comentários de aula

--- ContaPessoas/FirstViewController.h
+++ ContaPessoas/FirstViewController.h
@@ -1,8 +1,9 @@
 //
 //  FirstViewController.h
 //  ContaPessoas
+//  Esse programa é muito ruim
 //
-//  Created by Vinicius Miana on 2/18/15.
+//  Created by Isaías Lima on 02/19/15.
 //  Copyright (c) 2015 Vinicius Miana. All rights reserved.
 //
 
